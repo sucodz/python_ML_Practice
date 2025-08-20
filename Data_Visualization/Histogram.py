d8d3f85c0a3cbd2d923f8283c9072a6f39358dcc
Soulef Bentorki <93832597+sucodz@users.noreply.github.com>
2025-08-20 12:28:53 +0100

    Create Histogram.py

---
+++ Data_Visualization/Histogram.py
@@ -0,0 +1,7 @@
+# Histogram
+data = np.random.randn(1000)  # 1000 random values from normal distribution
+plt.hist(data, bins=30, color="skyblue", edgecolor="black")
+plt.title("Histogram Example")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
