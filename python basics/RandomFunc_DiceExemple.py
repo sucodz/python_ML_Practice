f5e91d0eef3b9727cd0f6eac7d489e28cbbb41f3
Soulef Bentorki <93832597+sucodz@users.noreply.github.com>
2025-08-22 11:45:37 +0100

    Update RandomFunc_DiceExemple.py

--- python basics/RandomFunc_DiceExemple.py
+++ python basics/RandomFunc_DiceExemple.py
@@ -1,20 +1,34 @@
 import numpy as np
-import numpy as np
-# Initialize random_walk
-random_walk = [0]
+import matplotlib.pyplot as plt
+# initialize and populate all_walks
+all_walks = []
+for i in range(5) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
 
-for x in range(100) :
-    step = random_walk[-1]
-    dice = np.random.randint(1,7)
+# Convert all_walks to NumPy array: np_aw
+np_aw =np.array(all_walks)
 
-    if dice <= 2:
-        # Replace below: use max to make sure step can't go below 0
-        step = max(0, step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1,7)
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
 
-    random_walk.append(step)
+# Clear the figure
+plt.clf()
 
-print(random_walk)
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
